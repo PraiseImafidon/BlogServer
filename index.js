cfc85296311efc4ce31d2aa48171c5845227defe
Praise Imafidon <108192872+PraiseImafidon@users.noreply.github.com>
2023-01-03 20:30:46 +0100

    Implemented sign up function

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
-import router from './routes/user-routes';
+import router from '../src/routes/user-routes';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
 //connect to database
-mongoose.connect('mongodb+srv://Ruona:techgirl@cluster0.jzy60.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = process.env.DB_URI
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to mongodb successfully!'))
     .catch((Error) => console.log(Error));
 
-
 //middleware
+app.use(express.json());
 app.use("/api/user", router);
+
 
 //Listen to the server
 const port = process.env.PORT || 3000;
